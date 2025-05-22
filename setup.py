6764154c582d0bfb5ab776b0af723a3b7d123215
A.Akhtari <AliAkhtari78@HotMail.com>
2025-05-22 16:51:08 -0400

    update project

--- setup.py
+++ setup.py
@@ -34,63 +34,63 @@ else:
 # Core dependencies
 install_requires = [
     # Core web scraping
-    'beautifulsoup4>=4.13.0,<5.0.0',
-    'lxml>=5.3.0,<6.0.0',
-    'requests>=2.32.0,<3.0.0',
-    'urllib3>=2.3.0,<3.0.0',
-    
+    'beautifulsoup4',
+    'lxml',
+    'requests',
+    'urllib3',
+
     # HTML/XML parsing
-    'cssselect>=1.2.0,<2.0.0',
-    'soupsieve>=2.5.0,<3.0.0',
-    
+    'cssselect',
+    'soupsieve',
+
     # Media processing
-    'eyeD3>=0.9.7,<1.0.0',
-    'filetype>=1.2.0,<2.0.0',
-    
+    'eyeD3',
+    'filetype',
+
     # Network utilities
-    'fake-useragent>=1.5.0,<2.0.0',
-    'certifi>=2024.2.2',
-    
+    'fake-useragent',
+    'certifi',
+
     # Configuration and data
-    'PyYAML>=6.0.1,<7.0.0',
-    'packaging>=24.0,<25.0.0',
-    'tqdm>=4.66.0,<5.0.0',
-    
+    'PyYAML',
+    'packaging',
+    'tqdm',
+
     # Parsing utilities
-    'pyparsing>=3.1.0,<4.0.0',
-    'deprecation>=2.1.0,<3.0.0',
+    'pyparsing',
+    'deprecation',
 ]
 
 # Optional dependencies
 extras_require = {
     'selenium': [
-        'selenium>=4.30.0,<5.0.0',
+        'selenium',
     ],
     'async': [
-        'pyppeteer>=2.0.0,<3.0.0',
-        'pyee>=11.0.0,<12.0.0',
-        'websockets>=12.0,<13.0.0',
+        'pyppeteer',
+        'pyee',
+        'websockets',
     ],
     'dev': [
-        'pytest>=8.0.0,<9.0.0',
-        'pytest-asyncio>=0.23.0,<1.0.0',
-        'pytest-cov>=4.0.0,<5.0.0',
-        'black>=24.0.0,<25.0.0',
-        'isort>=5.13.0,<6.0.0',
-        'flake8>=7.0.0,<8.0.0',
-        'mypy>=1.8.0,<2.0.0',
-        'pre-commit>=3.6.0,<4.0.0',
+        'pytest',
+        'pytest-asyncio',
+        'pytest-cov',
+        'black',
+        'isort',
+        'flake8',
+        'mypy',
+        'pre-commit',
     ],
     'docs': [
-        'sphinx>=7.0.0,<8.0.0',
-        'sphinx-rtd-theme>=2.0.0,<3.0.0',
-        'myst-parser>=2.0.0,<3.0.0',
+        'sphinx',
+        'sphinx-rtd-theme',
+        'myst-parser',
     ],
     'all': [
-        'selenium>=4.30.0,<5.0.0',
-        'pyppeteer>=2.0.0,<3.0.0',
-        'pyee>=11.0.0,<12.0.0',
-        'websockets>=12.0,<13.0.0',
+        'selenium',
+        'pyppeteer',
+        'pyee',
+        'websockets',
     ],
 }
 
@@ -100,12 +100,12 @@ setup(
     description='A modern Python library for extracting data from Spotify\'s web interface without using the official API',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    
+
     # Author and license
     author='Ali Akhtari',
     author_email='aliakhtari78@hotmail.com',
     license='MIT',
-    
+
     # URLs
     url='https://github.com/AliAkhtari78/SpotifyScraper',
     project_urls={
@@ -114,17 +114,17 @@ setup(
         'Documentation': 'https://spotifyscraper.readthedocs.io/',
         'Changelog': 'https://github.com/AliAkhtari78/SpotifyScraper/blob/main/CHANGELOG.md',
     },
-    
+
     # Package discovery
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     include_package_data=True,
-    
+
     # Requirements
     python_requires='>=3.8',
     install_requires=install_requires,
     extras_require=extras_require,
-    
+
     # Classifiers
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -144,27 +144,27 @@ setup(
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
-    
+
     keywords=[
-        'spotify', 'scraper', 'scraping', 'web-scraping', 'downloader', 
+        'spotify', 'scraper', 'scraping', 'web-scraping', 'downloader',
         'mp3', 'music', 'audio', 'cover', 'playlist', 'artist', 'album',
         'beautifulsoup', 'selenium', 'requests', 'automation'
     ],
-    
+
     # Entry points (if any CLI tools are added)
     entry_points={
         'console_scripts': [
             # 'spotifyscraper=spotify_scraper.cli:main',  # Uncomment if CLI is implemented
         ],
     },
-    
+
     # Package data
     package_data={
         'spotify_scraper': [
             'py.typed',  # Type hints marker
         ],
     },
-    
+
     # Zip safety
     zip_safe=False,
 )
